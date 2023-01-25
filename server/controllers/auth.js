4908fc308bd913c3e32e8cf0887f1b638f966098
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-01-25 21:57:54 +0500

    Register New User

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,10 @@
+import User from '../models/user';
+import { hashPassword, comparePassword } from '../helpers/auth';
+
 export const register = async (req, res) => {
   try {
     // validation
-    const { name, password } = req.body;
+    const { name, email, password } = req.body;
     if (!name) {
       return res.json({
         error: 'Name is required',
@@ -12,9 +15,30 @@ export const register = async (req, res) => {
         error: 'Password is required and should be 6 characters long',
       });
     }
-    res.json({
-      data: 'This is /register endpoint',
-    });
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: 'Email is taken',
+      });
+    }
+    // hash password
+    const hashedPassword = await hashPassword(password);
+
+    try {
+      const user = await new User({
+        name,
+        email,
+        password: hashedPassword,
+      }).save();
+
+      //   console.log(user);
+      const { password, ...rest } = user._doc;
+      return res.json({
+        user: rest,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   } catch (err) {
     console.log(err);
   }
