3188d941ea638aba2f2b9489fcdf78365eb54aef
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-01-26 22:39:13 +0500

    user login response

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/user';
 import { hashPassword, comparePassword } from '../helpers/auth';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
   try {
@@ -31,14 +32,52 @@ export const register = async (req, res) => {
         password: hashedPassword,
       }).save();
 
+      // create signed token
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      });
+
       //   console.log(user);
       const { password, ...rest } = user._doc;
       return res.json({
+        token,
         user: rest,
       });
     } catch (err) {
       console.log(err);
     }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // check email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.json({
+        error: 'No user found',
+      });
+    }
+    // check password
+    const match = await comparePassword(req.body.password, user.password);
+    if (!match) {
+      return res.json({
+        error: 'Wrong password',
+      });
+    }
+    // create signed token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+
+    const { password, ...rest } = user._doc;
+
+    res.json({
+      token,
+      user: rest,
+    });
   } catch (err) {
     console.log(err);
   }
