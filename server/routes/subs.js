12de42330b3977052b7ad05c8562e75e23d8beec
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-02-11 06:31:18 +0500

    Subscription status server

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -2,8 +2,15 @@ import express from 'express';
 
 const router = express.Router();
 
-import { prices } from '../controllers/subs';
+import {
+  prices,
+  createSubscription,
+  subscriptionStatus,
+} from '../controllers/subs';
+import { requireSignin } from '../middlewares';
 
 router.get('/prices', prices);
+router.post('/create-subscription', requireSignin, createSubscription);
+router.get('/subscription-status', requireSignin, subscriptionStatus);
 
 module.exports = router;
